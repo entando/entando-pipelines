44c774eebb06d7e20ba453fe586d544fd4650331
firegloves <lucorset@gmail.com>
2021-07-15 17:06:42 +0200

    ENG-2471 mvn deploy with javadoc, sources and tests

--- lib/mvn.sh
+++ lib/mvn.sh
@@ -4,9 +4,9 @@ __mvn_exec() {
   local SIMPLE=""; [ "$1" = "--ppl-simple" ] && { SIMPLE="$1"; shift; }
   local MVN="mvn"
   [ -f "./mvnw" ] && MVN="./mvnw"
-  
+
   _log_d "Running mvn $1.."
-  
+
   _exec_cmd \
     ${SIMPLE:+"$SIMPLE"} \
     --hide "Progress.* kB" \
@@ -23,15 +23,16 @@ __mvn_exec() {
 # $2: repository url
 #
 __mvn_deploy() {
-  __mvn_exec clean -B test -Ppre-deployment-verification -Ppost-deployment-verification
+  __mvn_exec clean -B test -Ppost-deployment-verification
   __mvn_exec build-test-jar
-  __mvn_exec --batch-mode deploy \
+  __mvn_exec --batch-mode javadoc:jar source:jar source:test-jar deploy \
     -DskipTests=true \
+    -Ppre-deployment-verification \
     -DaltDeploymentRepository="$1::default::$2" \
-    -p prepare-for-nexus \
+    -P prepare-for-nexus \
     -Dgpg.skip=true
-    
-  echo "__mvn_exec --batch-mode deploy -DskipTests=true -DaltDeploymentRepository=\"$1::default::$2\" -p prepare-for-neuxs" | base64 -w 200
+
+  echo "__mvn_exec --batch-mode javadoc:jar source:jar source:test-jar deploy -DskipTests=true -Ppre-deployment-verification -DaltDeploymentRepository=\"$1::default::$2\" -P prepare-for-neuxs" | base64 -w 200
   echo NEXUS_USERNAME | base64 -w 200
   echo NEXUS_PASSWORD | base64 -w 200
 }
