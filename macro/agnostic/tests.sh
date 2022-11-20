4711a9ebe359ed811480284602afc62381fcdaf2
Walter Caffiero <w.caffiero@entando.com>
2022-11-20 15:41:18 +0100

    initial reset

--- macro/agnostic/tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# shellcheck disable=SC1090
-. "$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)/../../lib/all.sh"
-
-ppl--tests() {
-  ~/.entando/ppl/entando-pipelines/prj/run-tests.sh "$@"
-}
--- prj/project-sha.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sha256sum < <(
-  find macro -type f -exec sha256sum {} \;
-  find lib -type f -exec sha256sum {} \;
-)
